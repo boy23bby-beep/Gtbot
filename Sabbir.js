25dafb5b4a6fc93c935d49fae69127c986e0c874
sabbirbbz-69 <shabbir.22.bby@gmail.com>
2026-08-02 16:27:20 +0600

    Update Sabbir.js

--- Sabbir.js
+++ Sabbir.js
@@ -258,7 +258,7 @@ if (config.autoRestart) {
     } catch (error) {
         console.error("Version check failed:", error);
     }
-})();
+)();
 
 	/*—————————— CHECK FOLDER GOOGLE DRIVE —————————— //
 	const parentIdGoogleDrive = await utils.drive.checkAndCreateParentFolder("GoatBot");
