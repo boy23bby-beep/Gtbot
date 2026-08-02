975b5b600c1a6afb77ef49bf5752d776f90e8482
sabbirbbz-69 <shabbir.22.bby@gmail.com>
2026-08-02 18:39:16 +0600

    Create server.js

---
+++ func/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Bot is running successfully!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
