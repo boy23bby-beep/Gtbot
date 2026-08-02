9c2e04106f8e50377d6a1139b04dbbd845029ed0
sabbirbbz-69 <shabbir.22.bby@gmail.com>
2026-08-02 17:30:10 +0600

    Update utils.js import path

--- Sabbir.js
+++ Sabbir.js
@@ -104,7 +104,7 @@ global.client = {
 	commandBanned: configCommands.commandBanned
 };
 
-const utils = require("./utils.js");
+const utils = require(".func/utils.js");
 global.utils = utils;
 const { colors } = utils;
 
